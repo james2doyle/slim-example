d4cf57f4710a5a1dd8fc73655a26799478c25beb
james2doyle <james2doyle@gmail.com>
2015-06-18 23:40:20 -0400

    add all the things

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$app = new \Slim\App();
+
+// Fetch DI Container
+$container = $app->getContainer();
+
+// Register Twig View service
+$container->register(new \Slim\Views\Twig(__DIR__ . '/views', [
+    'cache' => false // __DIR__ . '/cache'
+    ]));
+
+// Define named route
+$app->get('/', function ($request, $response, $args) {
+  return $this->view->render($response, 'welcome.twig.html', [
+    'title' => 'My Website'
+    ]);
+})->setName('homepage');
+
+// json test route
+$app->post('/hello', function ($request, $response) {
+  if (!$request->isAjax()) {
+    return $response->withStatus(403)->write("Method Not Allowed");
+  } else {
+    $data = $request->getParsedBody();
+    return $response->withHeader(
+      'Content-type',
+      'application/json'
+      )->write(json_encode(array(
+        'message' => "Hello, " . $data['name']
+        )));
+    }
+  })->setName('hello');
+
+$app->run();
